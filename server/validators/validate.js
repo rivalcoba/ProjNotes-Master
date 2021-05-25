0b31108f6de49dd873c2c2c7809268af6ff68635
rivalcoba <ivan.rivalcoba@gmail.com>
2021-05-25 14:09:13 -0500

    S45 Validacion de Proyectos

--- server/validators/validate.js
+++ server/validators/validate.js
@@ -1,24 +1,18 @@
-// Importando biblioteca de validación
-import * as Yup from 'yup';
-
+// Usando el patrón Factory para la creación de un middelware de validación
 const Validator = ({ shape, getObject }) => async (req, res, next) => {
-  // Construyendo el esquema
-  // const schema = Yup.object().shape(shape);
   // Construyendo el objeto de validación
   const dataObject = getObject(req);
   // El proceso de validación se encierra en un bloque try
   try {
     // Se valida el objeto
-    const validData = await shape.validate(dataObject);
-    console.log(`Valid DATA: ${validData}`);
+    const validData = await shape.validate(dataObject, { abortEarly: false });
     // Se inyecta el objeto validado en la petición
     req.validData = validData;
     // Se invoca al siguiente middleware en la cadena
     return next();
   } catch (error) {
     // En caso de error se regresa información de error
-    console.log("ERROR EN VALIDACION");
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error });
   }
 };
 // Exportando validador
