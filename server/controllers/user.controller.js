a1e4d55c54cb5ac3e65c625772fcf96981517dec
rivalcoba <ivan.rivalcoba@gmail.com>
2022-11-24 15:01:53 -0600

    Avance modelo de usuario

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,19 +5,32 @@ const login = (req, res) => {
   winston.info('Se manda a generar vista "user/login"');
   res.render('user/login');
 };
+
 // GET user/register
 const register = (req, res) => {
   winston.info('Se manda a generar vista "user/register"');
   res.render('user/register');
 };
+
 // POST user/register
 const registerUser = (req, res) => {
   // Extrayendo datos de validación
   const { validData, errorData } = req;
-  res.json({
-    validData,
-    errorData,
-  });
+  // Verificando si hay errores
+  if (errorData) {
+    // Respondemos el objeto de error
+    return res.json(errorData);
+  }
+  // Se crea al usuario
+  // con los datos provistos por el formulario
+  try {
+    return res.json({
+      validData,
+      errorData,
+    });
+  } catch (error) {
+    return res.json(error);
+  }
 };
 
 export default { login, register, registerUser };
